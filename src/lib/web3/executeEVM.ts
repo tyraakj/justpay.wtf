9bf997eba53ba78eff3102d2f9a3ef4218f7a9e5
tyraakj <tyra191712@gmail.com>
2026-06-15 17:21:32 +0530

    feat(evm): construct ERC-20 calldata and execute eth_sendTransaction

---
+++ src/lib/web3/executeEVM.ts
@@ -0,0 +1,56 @@
+import { encodeFunctionData, parseUnits } from 'viem'
+import { SendTransactionMutateAsync } from 'wagmi/query'
+
+// Minimal ERC-20 ABI for transfer
+const erc20Abi = [
+  {
+    "constant": false,
+    "inputs": [
+      { "name": "_to", "type": "address" },
+      { "name": "_value", "type": "uint256" }
+    ],
+    "name": "transfer",
+    "outputs": [{ "name": "", "type": "bool" }],
+    "type": "function"
+  }
+] as const
+
+interface ExecuteEVMParams {
+  sendTransactionAsync: SendTransactionMutateAsync<any, any>
+  tokenAddress: string | null
+  recipientAddress: `0x${string}`
+  amount: string
+  decimals: number
+}
+
+export async function executeEVMTransfer({
+  sendTransactionAsync,
+  tokenAddress,
+  recipientAddress,
+  amount,
+  decimals
+}: ExecuteEVMParams) {
+  const value = parseUnits(amount, decimals)
+
+  if (!tokenAddress) {
+    // Native ETH Transfer
+    const hash = await sendTransactionAsync({
+      to: recipientAddress,
+      value: value,
+    })
+    return hash
+  } else {
+    // ERC-20 Transfer
+    const data = encodeFunctionData({
+      abi: erc20Abi,
+      functionName: 'transfer',
+      args: [recipientAddress, value]
+    })
+    
+    const hash = await sendTransactionAsync({
+      to: tokenAddress as `0x${string}`,
+      data,
+    })
+    return hash
+  }
+}
